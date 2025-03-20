afc513a0090d341a68664137e0baf3f43f04fc13
Richa24vaidya <richa@devloops.in>
2025-03-20 11:30:28 -0700

    rc 1st commit 2nd file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+let accountState;
+
+
+accountEmail = "hc@hc.com"
+
+accountPassword = "21212121"
+accountCity = "vadodara"
+// accountid = 2  not allowednode 
+
+console.log(accountId);
+
+/*  preferred not to use var because of the issue  in the block of scope {} * and functional scope */
+ console.table([accountId, accountEmail,accountPassword,accountCity, accountState]);
